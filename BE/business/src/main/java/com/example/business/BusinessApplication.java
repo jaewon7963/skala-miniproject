affa665ece044b8c59eedc81f60a7892d6796b97
박현민 <hhyeonmin.park@gmail.com>
2026-09-03 22:55:23 +0900

    chore(core): 백엔드 실행 기반 정비

--- BE/business/src/main/java/com/example/business/BusinessApplication.java
+++ BE/business/src/main/java/com/example/business/BusinessApplication.java
@@ -3,7 +3,7 @@ package com.example.business;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.business", "com.logiccheck"})
 public class BusinessApplication {
 
 	public static void main(String[] args) {
